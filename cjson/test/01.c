88118cb8e9d2de6b2bfb411e7c793f194f61d892
zhouyang <zhouyang3456@outlook.com>
2025-06-13 17:04:41 +0800

    cJson

--- cjson/test/01.c
+++ cjson/test/01.c
@@ -100,6 +100,113 @@ end:
     return string;
 }
 
+// Alternatively we can use the cJSON_Add...ToObject helper functions to make our lives a little easier:
+
+//NOTE: Returns a heap allocated string, you are required to free it after use.
+char *create_monitor_with_helpers(void)
+{
+    const unsigned int resolution_numbers[2][2] = {
+        {1280, 720},
+        // {1920, 1080},
+        {3840, 2160}
+    };
+    char *string = NULL;
+    cJSON *resolutions = NULL;
+    size_t index = 0;
+
+    cJSON *monitor = cJSON_CreateObject();
+
+    if (cJSON_AddStringToObject(monitor, "name", "Awesome 4K") == NULL)
+    {
+        goto end;
+    }
+
+    resolutions = cJSON_AddArrayToObject(monitor, "resolutions");
+    if (resolutions == NULL)
+    {
+        goto end;
+    }
+
+    for (index = 0; index < (sizeof(resolution_numbers) / (2 * sizeof(int))); ++index)
+    {
+        cJSON *resolution = cJSON_CreateObject();
+
+        if (cJSON_AddNumberToObject(resolution, "width", resolution_numbers[index][0]) == NULL)
+        {
+            goto end;
+        }
+
+        if (cJSON_AddNumberToObject(resolution, "height", resolution_numbers[index][1]) == NULL)
+        {
+            goto end;
+        }
+
+        cJSON_AddItemToArray(resolutions, resolution);
+    }
+
+    string = cJSON_Print(monitor);
+    if (string == NULL)
+    {
+        fprintf(stderr, "Failed to print monitor.\n");
+    }
+
+end:
+    cJSON_Delete(monitor);
+    return string;
+}
+
+// Parsing
+// In this example we will parse a JSON in the above format and check if the monitor supports a Full HD resolution while printing some diagnostic output:
+
+/* return 1 if the monitor supports full hd, 0 otherwise */
+int supports_full_hd(const char * const monitor)
+{
+    const cJSON *resolution = NULL;
+    const cJSON *resolutions = NULL;
+    const cJSON *name = NULL;
+    int status = 0;
+    cJSON *monitor_json = cJSON_Parse(monitor);
+    if (monitor_json == NULL)
+    {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL)
+        {
+            fprintf(stderr, "Error before: %s\n", error_ptr);
+        }
+        status = 0;
+        goto end;
+    }
+
+    name = cJSON_GetObjectItemCaseSensitive(monitor_json, "name");
+    if (cJSON_IsString(name) && (name->valuestring != NULL))
+    {
+        printf("Checking monitor \"%s\"\n", name->valuestring);
+    }
+
+    resolutions = cJSON_GetObjectItemCaseSensitive(monitor_json, "resolutions");
+    cJSON_ArrayForEach(resolution, resolutions)
+    {
+        cJSON *width = cJSON_GetObjectItemCaseSensitive(resolution, "width");
+        cJSON *height = cJSON_GetObjectItemCaseSensitive(resolution, "height");
+
+        if (!cJSON_IsNumber(width) || !cJSON_IsNumber(height))
+        {
+            status = 0;
+            goto end;
+        }
+
+        if ((width->valuedouble == 1920) && (height->valuedouble == 1080))
+        {
+            status = 1;
+            goto end;
+        }
+    }
+
+end:
+    cJSON_Delete(monitor_json);
+    return status;
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
@@ -112,7 +219,25 @@ int main(int argc, char const *argv[])
 
     printf("Monitor JSON:\r\n%s\r\n", str);
 
+    str = create_monitor_with_helpers();
+    if (str == NULL)
+    {
+        fprintf(stderr, "Failed to create monitor with helpers.\n");
+        return 1;
+    }
+
+    printf("Monitor JSON with helpers:\r\n%s\r\n", str);
+
+    int full_hd = supports_full_hd(str);
+    if (full_hd)
+    {
+        printf("Monitor supports Full HD resolution.\n");
+    }
+    else
+    {
+        printf("Monitor does not support Full HD resolution.\n");
+    }
+
     return 0;
 }
-
 
